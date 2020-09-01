3b4b44af30500b9a82f4e81e573955833817f0ea
kaweesa01 <kawesa.moses02@gmail.com>
2020-09-01 21:57:07 +0300

    white noise

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@z@5kfi1^x7q#+-ycq#-xack-k59vfzqq6p98b5%eezj8p+#&4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://fast-beach-00008.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['https://activity-pad.herokuapp.com/']
 
 
 # Application definition
