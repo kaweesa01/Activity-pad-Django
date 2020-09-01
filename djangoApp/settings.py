98961d80af17a7132f0c3be5bcd68ba762081757
kaweesa01 <kawesa.moses02@gmail.com>
2020-09-01 21:30:30 +0300

    white noise

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoApp.urls'
@@ -121,5 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static/frontend')
+]
 
 STATIC_URL = '/static/'
