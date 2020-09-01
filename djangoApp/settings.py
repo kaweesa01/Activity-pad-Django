286dbf3b25676fee03bee03e2ac1fa8c80c4e541
kaweesa01 <kawesa.moses02@gmail.com>
2020-09-01 22:07:49 +0300

    white noise

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@z@5kfi1^x7q#+-ycq#-xack-k59vfzqq6p98b5%eezj8p+#&4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['activity-pad.herokuapp.com','localhost', '127.0.0.1']
 
